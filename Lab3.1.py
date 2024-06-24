5981babd5c4f8565540ad6e2a557e539e0a2b08f
Stang-S <6529011006@cdti.ac.th>
2024-06-24 14:43:36 +0700

    update

---
+++ Lab3.1.py
@@ -0,0 +1,11 @@
+x = 10
+while x>0:
+    print(x)
+    x-=1
+    
+print("__________________")
+
+y = 0
+while y<10:
+    print(y)
+    y+=2
---
+++ Lab4.2.py
@@ -0,0 +1,9 @@
+person = {"name":"Satchukorn","age":18, "wt" : 100, "h":173}
+#for n in person
+#    print(n)
+print(person["name"])
+print(person["age"])
+print(person["wt"])
+print(person["h"])
+person["name"] = "Satchukul"
+print(person["name"])
